e03054a8a713a0284e58d8a616eee6494d083576
Alex Lurye <allurye@nvidia.com>
2025-04-02 11:26:30 +0200

    Handle the case where multiple config-2 devices are present and wait for the right one

--- dib/ironic-python-agent-ramdisk/static/usr/local/bin/ironic-python-agent-resolve-configdrive.sh
+++ dib/ironic-python-agent-ramdisk/static/usr/local/bin/ironic-python-agent-resolve-configdrive.sh
@@ -11,34 +11,19 @@ PATH=/bin:/usr/bin:/sbin:/usr/sbin
 # result. In the case of VFAT partitions, they would be upper case.
 CONFIG_DRIVE_LABEL="config-2"
 
-# Wait until the device shows up
-while : ; do
-	# Identify the number of devices
-	device_count=$(lsblk -o PATH,LABEL | grep $CONFIG_DRIVE_LABEL | wc -l)
-	echo "device_count=$device_count"
-
-	[[ device_count -eq 0 ]] || break
-	sleep 5
-done
-
-set -eux
-set -o pipefail
-
 # Identify if we have an a publisher id set
 publisher_id=""
 if grep -q "ir_pub_id" /proc/cmdline; then
     publisher_id=$(cat /proc/cmdline | sed -e 's/^.*ir_pub_id=//' -e 's/ .*$//')
 fi
 
-if [ $device_count -lt 1 ]; then
-    # Nothing to do here, exit!
-    exit 0
-else
-    # We have *something* to do here.
-    mkdir -p /mnt/config
+mkdir -p /mnt/config
+
+while true ; do
+	echo "Looking for a device with publisher ID ${publisher_id} and label=${CONFIG_DRIVE_LABEL}"
     if [[ "${publisher_id}" != "" ]]; then
         # We need to enumerate through the devices, and obtain the
-        for device in $(lsblk -o PATH,LABEL|grep config-2|cut -f1 -d" "); do
+        for device in $(lsblk -o PATH,LABEL|grep ${CONFIG_DRIVE_LABEL}|cut -f1 -d" "); do
             device_id=$(udevadm info --query=property --property=ID_FS_PUBLISHER_ID $device | sed s/ID_FS_PUBLISHER_ID=//)
             if [[ "${publisher_id,,}" == "${device_id,,}" ]]; then
                 # SUCCESS! Valid device! Do it!
@@ -51,4 +36,5 @@ else
             fi
         done
     fi
-fi
+	sleep 5
+done
